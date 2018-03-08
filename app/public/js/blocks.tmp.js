2fd115f0f000e710d8d93a0f2513cde073e7c8f5
arcanrun <arcan.run@gmail.com>
2018-03-08 11:53:47 +0300

    fixes in header

--- app/public/js/blocks.tmp.js
+++ app/public/js/blocks.tmp.js
@@ -25,18 +25,6 @@ $(document).ready(function(){$('.about').waypoint(function() {
 );
 
 
-$('.header').waypoint(function() {
-  		console.log('waypoint:targets__inner:first-child');
-
-  	$('.header__text').addClass('animated bounceIn');
-  	 $('.header__img_1').addClass('animated bounceInLeft')
-
-  }, 
-    {
-   
-   context: document.getElementsByClassName('parllax') 
-	}
-);
 
 // $('.introduction').waypoint(function() {
 //   		console.log('waypoint');
