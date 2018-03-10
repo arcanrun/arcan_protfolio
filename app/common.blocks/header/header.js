b6fc9fbe1a011c208922ddef9b74b4b92d939b97
arcanrun <arcan.run@gmail.com>
2018-03-11 00:28:38 +0300

    fix in touch scroll

--- app/common.blocks/header/header.js
+++ app/common.blocks/header/header.js
@@ -2,11 +2,20 @@
 function onTuch(){
     if( device.mobile() || device.tablet() || device.ios() || device.ipad() || device.iphone() ||   device.ipod() || device.android()) {
         $('#scroller').kinetic({
-        x: false,
-        cursor: false
+	        x: false,
+	        cursor: false,
+	        filterTarget: function(target, e){
+	        
+	           if (!/down|start/.test(e.type))
+	           {
+	            return !(/area|a|input/i.test(target.tagName));
+	        }
+	        
+    }
     });
     } 
 }
 
 onTuch();
+
 
--- app/public/js/blocks.tmp.js
+++ app/public/js/blocks.tmp.js
@@ -29,13 +29,22 @@ $(document).ready(function(){$('.about').waypoint(function() {
 function onTuch(){
     if( device.mobile() || device.tablet() || device.ios() || device.ipad() || device.iphone() ||   device.ipod() || device.android()) {
         $('#scroller').kinetic({
-        x: false,
-        cursor: false
+	        x: false,
+	        cursor: false,
+	        filterTarget: function(target, e){
+	        
+	           if (!/down|start/.test(e.type))
+	           {
+	            return !(/area|a|input/i.test(target.tagName));
+	        }
+	        
+    }
     });
     } 
 }
 
 onTuch();
+
 
 
 // $('.introduction').waypoint(function() {
